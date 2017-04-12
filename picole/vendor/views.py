bb6c00a3e1dc68bc749f96dba966a0ee57df119c
Mateus Furquim <furquim.axs@gmail.com>
2017-04-12 19:32:46 -0300

    Popsicle view
    Signed-off-by: Mateus Furquim <furquim.axs@gmail.com>
    Signed-off-by: Parley Martins <parleypachecomartins@gmail.com>

--- picole/vendor/views.py
+++ picole/vendor/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 from vendor.models import Popsicle, Machine, Location, Stock, Transaction
 from vendor.serializers import PopsicleSerializer
 #, MachineSerializer, LocationSerializer, StockSerializer, TransactionSerializer
@@ -10,14 +11,23 @@ class PopsicleViewSet(viewsets.ModelViewSet):
 	serializer_class = PopsicleSerializer
 
 	def destroy(self, request, *args, **kwargs):
+		"""Set active attribute as false"""
+		return self.change_status(False, kwargs["pk"])
+
+	@detail_route(methods=['post'])
+	def reactivate(self, request, *args, **kwargs):
+		"""Set active attribute as true"""
+		return self.change_status(True, kwargs["pk"])
+
+	def change_status(self, active, popsicle_id):
 		try:
-			pop = Popsicle.objects.get(id=kwargs["pk"])
-			pop.is_active = False
+			pop = Popsicle.objects.get(id=popsicle_id)
+			pop.is_active = active
 			pop.save()
-			response = Response(status_code=status.HTTP_204_NO_CONTENT)
+			response = Response(status=status.HTTP_204_NO_CONTENT)
 		except Popsicle.DoesNotExist:
-			response = Response(status_code=status.HTTP_404_NOT_FOUND)
+			response = Response(status=status.HTTP_404_NOT_FOUND)
 		except Exception:
-			response = Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+			response = Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 		return response
