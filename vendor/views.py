8961bea73c9ee597f5c3b8a9bc9abf2009244a4b
Parley Martins <parleypachecomartins@gmail.com>
2017-07-07 12:58:58 -0300

    lasdjlfaks

--- vendor/views.py
+++ vendor/views.py
@@ -208,8 +208,8 @@ class PurchaseViewSet(viewsets.GenericViewSet):
         """
         purchases = models.Purchase.objects.filter(id__in=request.data['purchases'])
         popsicles = {}
+        url = 'http://picole.pagekite.me/'.format(p.machine.ip)
         for p in purchases:
-            url = 'http://picole.pagekite.me/'.format(p.machine.ip)
             popsicles[p.popsicle.id] = {
                 'release': p.amount,
                 'new_amount': p.machine.stocks.get(popsicle_id=p.popsicle.id).amount
